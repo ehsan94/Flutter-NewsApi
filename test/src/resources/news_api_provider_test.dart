655aff35ba9ec43df207a76f541375b47135d421
Ehsan <ehsan@user12s-MacBook-Pro.local>
2020-08-10 17:31:54 +0200

    Commiting

---
+++ test/src/resources/news_api_provider_test.dart
@@ -0,0 +1,33 @@
+import 'package:news/src/resources/news_api_provider.dart';
+import 'dart:convert';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+
+void main() {
+  test('Fetch top ids', () async {
+    //setup of test case
+    final newsApi = NewsApiProvider();
+
+    newsApi.client = MockClient((request) async {
+      return Response(json.encode([1, 2, 3, 4]), 200);
+    });
+    //expectation
+    final ids = await newsApi.fetchTopIds();
+
+    expect(ids, [1, 2, 3, 4]);
+  });
+
+  test('Fetch detail of item', () async {
+    final newsApi = NewsApiProvider();
+
+    newsApi.client = MockClient((request) async {
+      final jsonMap = {'id': 123};
+
+      return Response(json.encode(jsonMap), 200);
+
+      final item = await newsApi.fetchItem(999);
+      expect(item, 123);
+    });
+  });
+}
