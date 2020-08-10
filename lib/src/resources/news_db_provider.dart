907693bead8a151aa3b49f805f48cb12836f1540
Ehsan <ehsan@user12s-MacBook-Pro.local>
2020-08-10 21:33:57 +0200

    abstract class for source and cache

--- lib/src/resources/news_db_provider.dart
+++ lib/src/resources/news_db_provider.dart
@@ -4,9 +4,14 @@ import 'dart:io';
 import 'package:path/path.dart';
 import 'dart:async';
 import '../models/item_model.dart';
+import 'repository.dart';
 
-class NewsDbProvider{
+class NewsDbProvider implements Source, Cache{
   Database db;
+
+  NewsDbProvider(){
+    init();
+  }
 
   void init() async{
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
@@ -54,4 +59,12 @@ class NewsDbProvider{
   Future<int> addItem(ItemModel item){
    return db.insert('Items', item.toMap());
   }
+
+  @override
+  Future<List<int>> fetchTopIds() {
+    // TODO: implement fetchTopIds
+    throw UnimplementedError();
+  }
 }
+
+final newsDbProvider = NewsDbProvider();
